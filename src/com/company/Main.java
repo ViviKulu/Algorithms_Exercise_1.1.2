fc562920fde12f2c35f1cc7711623a5ed8ea75bc
Vivian Babirye <babimaji1111@gmail.com>
2018-12-19 22:38:13 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    //    Give the type and value of each of the following expressions:
+
+    //    a. (1 + 2.236)/2
+    //    b. 1 + 2 + 3 + 4.0
+    //    c. 4.1 >= 4
+    //    d. 1 + 2 + "3"
+
+    //    Answers:
+
+    //    a. (1 + 2.236)/2 = 1.618
+    //    b. 1 + 2 + 3 + 4.0 = 10
+    //    c. 4.1 >= 4 =  true
+    //    d. 1 + 2 + "3" = 33
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
